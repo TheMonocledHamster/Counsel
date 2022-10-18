0394c83e0c81643175c0245d55f2a1be63f11e9e
Adithya Hegde <adithyah07@gmail.com>
2022-10-18 11:38:00 +0530

    Add outline for custom env

--- rl/custom_env.py
+++ rl/custom_env.py
@@ -3,4 +3,27 @@ import gym
 class CustomEnv(gym.Env):
     def __init__(self) -> None:
         super().__init__()
-        
+    
+    def preprocess(self):
+        pass
+
+    def step(self):
+        self
+    
+    def reset(self):
+        pass
+
+    def get_obs(self):
+        pass
+
+    def terminate(self):
+        pass
+
+    def save_if_best(self):
+        pass
+
+    def is_visited(self):
+        pass
+
+    def save_trajectory(self):
+        pass
