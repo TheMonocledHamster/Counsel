0236c8f43be4d34381dc6840a92459c72237ce2b
Adithya Hegde <adithyah07@gmail.com>
2022-11-25 02:24:22 +0530

    testing emulates real

--- source/tester.py
+++ source/tester.py
@@ -2,6 +2,7 @@ import json
 from typing import List
 import urllib.request as request
 import numpy as np
+import time
 
 def set_slo(slo:int):
     """
@@ -17,22 +18,37 @@ def call_load_server(choice:int,
                      )->List[
                              List[int],
                              np.array[float],
-                             float,
-                             int,
-                             int
+                             float
                             ]:
     """
     Call the server with the action and get the next metrics.
     """
-    url = "http://localhost:8000/"
-    data = json.dumps({"choice": choice}).encode("utf-8")
-    req = request.Request(url, data=data, method="POST")
-    with request.urlopen(req) as f:
-        metrics = json.loads(f.read().decode("utf-8"))
-    
-    ucpu = np.array(metrics["resources"][0])
-    umem = np.array(metrics["resources"][1])
-    util = np.sqrt(np.sum((ucpu/cpu)**2) + np.sum((umem/mem)**2))
-    
-    return [metrics["arrival_rate"], util, metrics["latency"], 
-            metrics["action_type"], metrics["action_component"]]
+    while True:
+        url = "http://localhost:8000/"
+        data = json.dumps({"choice": choice}).encode("utf-8")
+        req = request.Request(url, data=data, method="POST")
+        with request.urlopen(req) as f:
+            metrics = json.loads(f.read().decode("utf-8"))
+        arrival_rate = np.array(metrics["arrival_rate"])
+        latency = metrics["latency"]
+
+        ucpu = np.array(metrics["resources"][0])
+        umem = np.array(metrics["resources"][1])
+        util = np.sqrt(np.sum((ucpu/cpu)**2) + np.sum((umem/mem)**2))
+
+        act_type = 0
+        act_comp = -1
+        flag = False
+        for i in range(len(cpu)):
+            if ucpu[i] > cpu[i] or umem[i] > mem[i]:
+                flag = True
+                act_type = 1
+                act_comp = i
+            elif ucpu[i] < cpu[i]/2 and umem[i] < mem[i]/2 and act_comp == -1:
+                flag = True
+                act_comp = i
+
+        if flag:
+            return [arrival_rate, util, latency, act_type, act_comp]
+        
+        time.sleep(5)
