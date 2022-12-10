e4ac841ca29768ad00fe97221fc6aa1b561829a3
Adithya Hegde <adithyah07@gmail.com>
2022-12-10 21:21:12 +0530

    hotfix

--- vary_configs.sh
+++ vary_configs.sh
@@ -1,23 +1,23 @@
 # python ./train.py -n std-f5-c3 -ncf 5 -ncp 3 -e 1000
-python ./train.py -n std-f10-c3 -ncf 10 -ncp 3 -e 1000
-python ./train.py -n std-f25-c3 -ncf 25 -ncp 3 -e 1000
-python ./train.py -n std-f50-c3 -ncf 50 -ncp 3 -e 1000
-python ./train.py -n std-f100-c3 -ncf 100 -ncp 3 -e 1000
+python ./train.py -n std-f10-c3 -ncf 10 -ncp 3 -e 500
+python ./train.py -n std-f25-c3 -ncf 25 -ncp 3 -e 500
+python ./train.py -n std-f50-c3 -ncf 50 -ncp 3 -e 500
+python ./train.py -n std-f100-c3 -ncf 100 -ncp 3 -e 500
 
-python ./train.py -n std-f5-5 -ncf 5 -ncp 5 -e 1000
+python ./train.py -n std-f5-5 -ncf 5 -ncp 5 -e 500
 # python ./train.py -n std-f10-c5 -ncf 10 -ncp 5
 # python ./train.py -n std-f25-c5 -ncf 25 -ncp 5
 # python ./train.py -n std-f50-c5 -ncf 50 -ncp 5
-python ./train.py -n std-f100-c5 -ncf 100 -ncp 5 -e 1000
+python ./train.py -n std-f100-c5 -ncf 100 -ncp 5 -e 500
 
-python ./train.py -n std-f5-c10 -ncf 5 -ncp 10 -e 1000
+python ./train.py -n std-f5-c10 -ncf 5 -ncp 10 -e 500
 # python ./train.py -n std-f10-c10 -ncf 10 -ncp 10
 # python ./train.py -n std-f25-c10 -ncf 25 -ncp 10
 # python ./train.py -n std-f50-c10 -ncf 50 -ncp 10
-python ./train.py -n std-f100-c10 -ncf 100 -ncp 10 -e 1000
+python ./train.py -n std-f100-c10 -ncf 100 -ncp 10 -e 500
 
-python ./train.py -n std-f5-c20 -ncf 5 -ncp 20 -e 1000
+python ./train.py -n std-f5-c20 -ncf 5 -ncp 20 -e 500
 # python ./train.py -n std-f10-c20 -ncf 10 -ncp 20
 # python ./train.py -n std-f25-c20 -ncf 25 -ncp 20
 # python ./train.py -n std-f50-c20 -ncf 50 -ncp 20
-python ./train.py -n std-f100-c20 -ncf 100 -ncp 20 -e 1000
+python ./train.py -n std-f100-c20 -ncf 100 -ncp 20 -e 500
